86e2fc76f67576f8357acbd6f9df74104a8dadef
Phuc Long Nguyen <90314029+longcg18@users.noreply.github.com>
2023-07-04 17:02:16 +0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -144,3 +144,14 @@ if __name__ == '__main__':
     i, c = input().split()
     s_new = mutate_string(s, int(i), c)
     print(s_new)
+
+
+### List Comprehensions
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    
+    print([[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if sum([i, j, k]) != n])
